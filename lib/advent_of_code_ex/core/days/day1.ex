3553f134a45ab23d1a809da1c69df152dd0831fb
corsen-olbin <corbinolsen97@gmail.com>
2023-12-01 00:49:00 -0600

    day 1

--- lib/advent_of_code_ex/core/days/day1.ex
+++ lib/advent_of_code_ex/core/days/day1.ex
@@ -1,11 +1,46 @@
 defmodule AdventOfCodeEx.Core.Days.Day1 do
 
 
-  def part_1(_input) do
-    :unimplemented
+  def part_1(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.map(&map_to_freq/1)
+    |> Enum.sum()
   end
 
-  def part_2(_input) do
-    :unimplemented
+  def part_2(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.map(&map_to_freq2/1)
+    |> Enum.sum()
+  end
+
+  def map_to_freq(str) do
+    Regex.scan(~r/([0-9]{1})/, str, capture: :first)
+    |> List.flatten
+    |> then(fn x -> List.first(x) <> List.last(x) end)
+    |> String.to_integer()
+  end
+
+  def map_to_freq2(str) do
+    Regex.scan(~r/(?=([0-9]{1}|one|two|three|four|five|six|seven|eight|nine))/, str)
+    |> Enum.map(fn [_x, y] -> y end)
+    |> then(fn x -> to_num(List.first(x)) <> to_num(List.last(x)) end)
+    |> String.to_integer()
+  end
+
+  def to_num(str) do
+    case str do
+      "one" -> "1"
+      "two" -> "2"
+      "three" -> "3"
+      "four" -> "4"
+      "five" -> "5"
+      "six" -> "6"
+      "seven" -> "7"
+      "eight" -> "8"
+      "nine" -> "9"
+      x -> x
+    end
   end
 end
